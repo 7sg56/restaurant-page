337e8f98fc6b9f3db60907db8eeff07a6c3f3bc0
7sg56 <sghosh.ile.7@gmail.com>
2024-11-25 21:50:04 +0530

    Update 1.3 - added responsiveness and animations

--- script.js
+++ script.js
@@ -3,3 +3,33 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", window.scrollY > 80);
 });
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '45px',
+    duration: 1500,
+    reset: true
+})
+
+sr.reveal ('.home-text',{delay:200});
+sr.reveal ('.home-img',{delay:300});
+
+
+sr.reveal ('.about-img',{});
+sr.reveal ('.about-text',{delay:200});
+
+sr.reveal ('.middle-text',{});
+sr.reveal ('.row-btn,.shop-content',{delay:200});
